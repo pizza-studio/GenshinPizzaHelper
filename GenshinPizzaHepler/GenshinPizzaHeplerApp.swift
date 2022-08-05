a2226b18f4246a1dcefc47e6312c8f519f5f3a4c
Lava <2627403074@qq.com>
2022-08-05 22:25:31 +0800

    Initial Commit

---
+++ GenshinPizzaHepler/GenshinPizzaHeplerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenshinPizzaHeplerApp.swift
+//  GenshinPizzaHepler
+//
+//  Created by 戴藏龙 on 2022/7/19.
+//
+
+import SwiftUI
+
+@main
+struct GenshinPizzaHeplerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
