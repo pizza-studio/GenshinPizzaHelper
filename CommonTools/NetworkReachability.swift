bf7a3ed86f89b4b865c54dd964d6ec42e707aee1
Bill-Haku <battleandwar@hotmail.com>
2022-08-06 11:20:12 +0800

    feat: add network reachability checker

---
+++ CommonTools/NetworkReachability.swift
@@ -0,0 +1,33 @@
+//
+//  NetworkReachability.swift
+//  原神披萨小助手
+//
+//  Created by Bill Haku on 2022/8/6.
+//
+
+import Foundation
+import SystemConfiguration
+
+class NetworkReachability: ObservableObject {
+    @Published private(set) var reachable: Bool = false
+    private let reachability = SCNetworkReachabilityCreateWithName(nil, "https://api-takumi-record.mihoyo.com")
+
+    init() {
+        self.reachable = checkConnection()
+    }
+
+    private func isNetworkReachable(with flags: SCNetworkReachabilityFlags) -> Bool {
+        let isReachable = flags.contains(.reachable)
+        let connectionRequired = flags.contains(.connectionRequired)
+        let canConnectAutomatically = flags.contains(.connectionOnDemand) || flags.contains(.connectionOnTraffic)
+        let canConnectWithoutIntervention = canConnectAutomatically && !flags.contains(.interventionRequired)
+        return isReachable && (!connectionRequired || canConnectWithoutIntervention)
+    }
+
+    func checkConnection() -> Bool {
+        var flags = SCNetworkReachabilityFlags()
+        SCNetworkReachabilityGetFlags(reachability!, &flags)
+
+        return isNetworkReachable(with: flags)
+    }
+}
