611c41a9269f9707512ae1b06b30bf7d7c4c2c7b
Bill-Haku <battleandwar@hotmail.com>
2022-08-10 09:45:13 +0800

    feat: add policy in helpview

--- GenshinPizzaHepler/View/HelpSheetView.swift
+++ GenshinPizzaHepler/View/HelpSheetView.swift
@@ -17,6 +17,9 @@ struct HelpSheetView: View {
                     Link("获取Cookie的脚本", destination: URL(string: "https://www.icloud.com/shortcuts/fe68f22c624949c9ad8959993239e19c")!)
                 }
                 Section {
+                    NavigationLink(destination: WebBroswerView(url: "http://zhuaiyuwen.xyz/static/policy.html")) {
+                        Text("用户协议与免责声明")
+                    }
                     NavigationLink(destination: AboutView()) {
                         Text("关于小助手")
                     }
