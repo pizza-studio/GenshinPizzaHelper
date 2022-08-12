f8af471ac772c8d45abb3f73bb179facd4979f19
Bill-Haku <battleandwar@hotmail.com>
2022-08-12 14:46:02 +0800

    fix: user policy add navigation title

--- GenshinPizzaHepler/View/HelpSheetView.swift
+++ GenshinPizzaHepler/View/HelpSheetView.swift
@@ -17,7 +17,7 @@ struct HelpSheetView: View {
                     Link("获取Cookie的脚本", destination: URL(string: "https://www.icloud.com/shortcuts/fe68f22c624949c9ad8959993239e19c")!)
                 }
                 Section {
-                    NavigationLink(destination: WebBroswerView(url: "http://zhuaiyuwen.xyz/static/policy.html")) {
+                    NavigationLink(destination: WebBroswerView(url: "http://zhuaiyuwen.xyz/static/policy.html").navigationTitle("用户协议")) {
                         Text("用户协议与免责声明")
                     }
                     NavigationLink(destination: WebBroswerView(url: "http://zhuaiyuwen.xyz/static/donate.html").navigationTitle("支持我们")) {
