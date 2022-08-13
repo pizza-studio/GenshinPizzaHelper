7e764934560281433e3bbe4dc81aed3a79902dc1
Bill-Haku <battleandwar@hotmail.com>
2022-08-13 10:02:53 +0800

    feat: add App Store review in help sheet

--- CommonTools/ReviewHandler.swift
+++ CommonTools/ReviewHandler.swift
@@ -12,7 +12,7 @@ import SwiftUI
 class ReviewHandler {
 
     static func requestReview() {
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2.0) {
+        DispatchQueue.main.async {
             if let scene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
                 SKStoreReviewController.requestReview(in: scene)
             }
--- GenshinPizzaHepler/View/HelpSheetView.swift
+++ GenshinPizzaHepler/View/HelpSheetView.swift
@@ -17,11 +17,16 @@ struct HelpSheetView: View {
                     Link("获取Cookie的脚本", destination: URL(string: "https://www.icloud.com/shortcuts/fe68f22c624949c9ad8959993239e19c")!)
                 }
                 Section {
-                    NavigationLink(destination: WebBroswerView(url: "http://zhuaiyuwen.xyz/static/policy.html").navigationTitle("用户协议")) {
-                        Text("用户协议与免责声明")
+                    Button("在App Store评分") {
+                        ReviewHandler.requestReview()
                     }
                     NavigationLink(destination: WebBroswerView(url: "http://zhuaiyuwen.xyz/static/donate.html").navigationTitle("支持我们")) {
                         Text("支持我们")
+                    }
+                }
+                Section {
+                    NavigationLink(destination: WebBroswerView(url: "http://zhuaiyuwen.xyz/static/policy.html").navigationTitle("用户协议")) {
+                        Text("用户协议与免责声明")
                     }
                     NavigationLink(destination: AboutView()) {
                         Text("关于小助手")
