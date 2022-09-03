f7e440460dc4d342077211d38b0063318157e361
Lava <2627403074@qq.com>
2022-09-03 16:14:54 +0800

    pref: percentage display of transformer

--- Model/FetchedModels/Detail/TransformerInfo.swift
+++ Model/FetchedModels/Detail/TransformerInfo.swift
@@ -13,7 +13,20 @@ struct TransformerInfo {
     
     var isComplete: Bool { recoveryTime.isComplete }
     
-    var percentage: Double { (604800.0 - Double(recoveryTime.second)) / 604800.0 }
+    var percentage: Double {
+        // 因为返回的只会精准到天，所以多加十二个小时以符合直觉
+        if recoveryTime.second > (24 * 60 * 60) {
+            let pct = (604800.0 - Double(recoveryTime.second + 12 * 60 * 60)) / 604800.0
+            if  pct > 1 {
+                return 1.0
+            } else {
+                return pct
+            }
+        } else {
+            return (604800.0 - Double(recoveryTime.second)) / 604800.0
+        }
+    }
+
     
     init(_ transformerData: TransformerData) {
         self.obtained = transformerData.obtained
