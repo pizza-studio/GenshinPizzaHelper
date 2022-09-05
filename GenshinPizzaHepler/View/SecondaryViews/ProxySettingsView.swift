6c3bac59f931d445f90ff31622bbb46518c66457
Bill-Haku <battleandwar@hotmail.com>
2022-09-05 20:49:18 +0800

    feat: add username & password in proxy settings

--- GenshinPizzaHepler/View/SecondaryViews/ProxySettingsView.swift
+++ GenshinPizzaHepler/View/SecondaryViews/ProxySettingsView.swift
@@ -11,6 +11,8 @@ struct ProxySettingsView: View {
     @AppStorage("useProxy", store: UserDefaults(suiteName: "group.GenshinPizzaHelper")) var useProxy: Bool = false
     @AppStorage("proxyHost", store: UserDefaults(suiteName: "group.GenshinPizzaHelper")) var proxyHost: String = ""
     @AppStorage("proxyPort", store: UserDefaults(suiteName: "group.GenshinPizzaHelper")) var proxyPort: String = ""
+    @AppStorage("proxyUserName", store: UserDefaults(suiteName: "group.GenshinPizzaHelper")) var proxyUserName: String = ""
+    @AppStorage("proxyUserPassword", store: UserDefaults(suiteName: "group.GenshinPizzaHelper")) var proxyPassword: String = ""
 
     var body: some View {
         List {
@@ -20,6 +22,10 @@ struct ProxySettingsView: View {
             Section (header: Text("代理配置")) {
                 InfoEditor(title: "服务器", content: $proxyHost)
                 InfoEditor(title: "端口", content: $proxyPort, keyboardType: .numberPad)
+            }
+            Section (header: Text("认证信息（可选）")) {
+                InfoEditor(title: "用户名", content: $proxyUserName)
+                InfoEditor(title: "密码", content: $proxyPassword)
             }
         }
         .navigationTitle("代理设置")
