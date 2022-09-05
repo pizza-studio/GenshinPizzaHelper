a7380809d1a3cc0895182f00ed5859ca8dfbc206
Bill-Haku <battleandwar@hotmail.com>
2022-09-05 19:49:35 +0800

    feat: add proxy host & port in proxy settings

--- GenshinPizzaHepler/View/SecondaryViews/ProxySettingsView.swift
+++ GenshinPizzaHepler/View/SecondaryViews/ProxySettingsView.swift
@@ -9,11 +9,17 @@ import SwiftUI
 
 struct ProxySettingsView: View {
     @AppStorage("useProxy", store: UserDefaults(suiteName: "group.GenshinPizzaHelper")) var useProxy: Bool = false
+    @AppStorage("proxyHost", store: UserDefaults(suiteName: "group.GenshinPizzaHelper")) var proxyHost: String = ""
+    @AppStorage("proxyPort", store: UserDefaults(suiteName: "group.GenshinPizzaHelper")) var proxyPort: String = ""
 
     var body: some View {
         List {
             Section {
                 Toggle("启用代理", isOn: $useProxy)
+            }
+            Section (header: Text("代理配置")) {
+                InfoEditor(title: "服务器", content: $proxyHost)
+                InfoEditor(title: "端口", content: $proxyPort, keyboardType: .numberPad)
             }
         }
         .navigationTitle("代理设置")
