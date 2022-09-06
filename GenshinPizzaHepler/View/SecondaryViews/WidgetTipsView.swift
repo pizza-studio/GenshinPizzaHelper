ac839d85b684d1440fde9909b59835a8583c9a37
Bill-Haku <battleandwar@hotmail.com>
2022-09-06 16:18:08 +0800

    feat: adjust widget tips view ui

--- GenshinPizzaHepler/View/SecondaryViews/WidgetTipsView.swift
+++ GenshinPizzaHepler/View/SecondaryViews/WidgetTipsView.swift
@@ -11,16 +11,20 @@ struct WidgetTipsView: View {
     var body: some View {
         NavigationView {
             List {
-                Section (header: Text("如何配置小组件？")) {
+                Section (header: Text("如何配置小组件和修改小组件背景？")) {
                     Label("长按小组件，选择编辑\"原神披萨小助手\"", systemImage: "1.circle")
-                    Label("根据提示选择设置项或启用/关闭功能", systemImage: "2.circle")
+                        .padding(.vertical)
+                    Label("根据提示选择设置项或启用/关闭功能或修改背景", systemImage: "2.circle")
+                        .padding(.vertical)
                 }
 
                 Section (header: Text("如何添加小组件？")) {
                     NavigationLink(destination: WebBroswerView(url: "https://support.apple.com/guide/iphone/iphb8f1bf206/").navigationTitle("如何添加小组件？").navigationBarTitleDisplayMode(.inline)) {
                         Label("关于如何添加小组件，请参考Apple支持文档", systemImage: "safari")
+                            .padding(.vertical)
                     }
                     Label("如果您未能在小组件选单内找到本软件的小组件，请重启并等待十分钟后再尝试添加小组件", systemImage: "exclamationmark.circle")
+                        .padding(.vertical)
                 }
             }
             .navigationTitle("小组件使用提示")
