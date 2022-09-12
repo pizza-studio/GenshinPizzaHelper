46604c7ab8981895a77778c125b00957ca766349
Lava <2627403074@qq.com>
2022-09-12 20:27:15 +0800

    pref: color

--- ResinStatusWidget/LockScreenWidgets/LockScreenResinWidget/LockScreenResinWidgetRectangular.swift
+++ ResinStatusWidget/LockScreenWidgets/LockScreenResinWidget/LockScreenResinWidgetRectangular.swift
@@ -14,46 +14,97 @@ struct LockScreenResinWidgetRectangular: View {
     let result: FetchResult
 
     var body: some View {
-        switch result {
-        case .success(let data):
-            HStack {
-                VStack(alignment: .leading) {
-                    HStack(alignment: .lastTextBaseline, spacing: 2) {
-                        Text("\(data.resinInfo.currentResin)")
-                            .font(.system(size: 30, design: .rounded))
-                        Text("\(Image("icon.resin"))")
-                            .font(.system(size: 20, design: .rounded))
+        switch widgetRenderingMode {
+        case .fullColor:
+            switch result {
+            case .success(let data):
+                HStack {
+                    VStack(alignment: .leading) {
+                        HStack(alignment: .lastTextBaseline, spacing: 2) {
+                            Text("\(data.resinInfo.currentResin)")
+                                .font(.system(size: 30, design: .rounded))
+                            Text("\(Image("icon.resin"))")
+                                .font(.system(size: 20, design: .rounded))
+                        }
+                        .widgetAccentable()
+                        .foregroundColor(Color("iconColor.resin.middle"))
+                        if data.resinInfo.isFull {
+                            Text("已回满")
+                                .font(.footnote)
+                                .fixedSize(horizontal: false, vertical: true)
+                        } else {
+                            Text("\(data.resinInfo.recoveryTime.completeTimePointFromNow()) 回满")
+                                .font(.footnote)
+                                .fixedSize(horizontal: false, vertical: true)
+                                .foregroundColor(.gray)
+                        }
                     }
-                    .widgetAccentable()
-                    if data.resinInfo.isFull {
-                        Text("已回满")
-                            .font(.footnote)
-                            .fixedSize(horizontal: false, vertical: true)
-                    } else {
-                        Text("\(data.resinInfo.recoveryTime.completeTimePointFromNow()) 回满")
-                            .font(.footnote)
-                            .fixedSize(horizontal: false, vertical: true)
-                    }
+                    Spacer()
                 }
-                Spacer()
+            case .failure(_):
+                HStack {
+                    VStack(alignment: .leading) {
+                        HStack(alignment: .lastTextBaseline, spacing: 2) {
+                            Text("\(Image(systemName: "ellipsis"))")
+                                .font(.system(size: 30, design: .rounded))
+                            Text("\(Image("icon.resin"))")
+                                .font(.system(size: 20, design: .rounded))
+                        }
+                        .widgetAccentable()
+                        .foregroundColor(.cyan)
+                        Text(Image("icon.resin"))
+                            .font(.footnote)
+                            .fixedSize(horizontal: false, vertical: true)
+                    }
+                    Spacer()
+                }
             }
-        case .failure(_):
-            HStack {
-                VStack(alignment: .leading) {
-                    HStack(alignment: .lastTextBaseline, spacing: 2) {
-                        Text("\(Image(systemName: "ellipsis"))")
-                            .font(.system(size: 30, design: .rounded))
-                        Text("\(Image("icon.resin"))")
-                            .font(.system(size: 20, design: .rounded))
+        default:
+            switch result {
+            case .success(let data):
+                HStack {
+                    VStack(alignment: .leading) {
+                        HStack(alignment: .lastTextBaseline, spacing: 2) {
+                            Text("\(data.resinInfo.currentResin)")
+                                .font(.system(size: 30, design: .rounded))
+                            Text("\(Image("icon.resin"))")
+                                .font(.system(size: 20, design: .rounded))
+                        }
+                        .widgetAccentable()
+                        if data.resinInfo.isFull {
+                            Text("已回满")
+                                .font(.footnote)
+                                .fixedSize(horizontal: false, vertical: true)
+                                .foregroundColor(.gray)
+                        } else {
+                            Text("\(data.resinInfo.recoveryTime.completeTimePointFromNow()) 回满")
+                                .font(.footnote)
+                                .fixedSize(horizontal: false, vertical: true)
+                                .foregroundColor(.gray)
+                        }
                     }
-                    .widgetAccentable()
-                    Text(Image("icon.resin"))
-                        .font(.footnote)
-                        .fixedSize(horizontal: false, vertical: true)
+                    Spacer()
                 }
-                Spacer()
+            case .failure(_):
+                HStack {
+                    VStack(alignment: .leading) {
+                        HStack(alignment: .lastTextBaseline, spacing: 2) {
+                            Text("\(Image(systemName: "ellipsis"))")
+                                .font(.system(size: 30, design: .rounded))
+                            Text("\(Image("icon.resin"))")
+                                .font(.system(size: 20, design: .rounded))
+                        }
+                        .widgetAccentable()
+                        Text(Image("icon.resin"))
+                            .font(.footnote)
+                            .fixedSize(horizontal: false, vertical: true)
+                            .foregroundColor(.gray)
+                    }
+                    Spacer()
+                }
             }
         }
+
     }
 }
 
