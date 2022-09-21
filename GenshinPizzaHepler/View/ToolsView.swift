429f6218b1a561d6e72e40bff84c56bf69373b83
Bill-Haku <battleandwar@hotmail.com>
2022-09-21 16:49:58 +0800

    feat: add tool view contents

--- GenshinPizzaHepler/View/ToolsView.swift
+++ GenshinPizzaHepler/View/ToolsView.swift
@@ -9,6 +9,34 @@ import SwiftUI
 
 struct ToolsView: View {
     var body: some View {
-        Text("敬请期待")
+        NavigationView {
+            List {
+                Section(header: Text("我的角色")) {
+                    ScrollView(.horizontal) {
+                        HStack {
+                            Image(systemName: "safari")
+                                .resizable()
+                                .frame(width: 50, height: 50)
+                            Image(systemName: "safari")
+                                .resizable()
+                                .frame(width: 50, height: 50)
+                            Image(systemName: "safari")
+                                .resizable()
+                                .frame(width: 50, height: 50)
+                        }
+                        .padding(.vertical)
+                    }
+                    .padding(.horizontal)
+                    .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
+                    .listRowBackground(Color(UIColor.secondarySystemBackground).opacity(0.6))
+                }
+                Section(header: Text("第三方工具")) {
+                    Text("原神中日英词典")
+                }
+            }
+            .listStyle(.plain)
+            .navigationTitle("原神小工具")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
