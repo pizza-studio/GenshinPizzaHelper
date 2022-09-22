bd5650530ace4d4ab180b48b9ba66c1eafcc4789
Lava <2627403074@qq.com>
2022-09-22 16:38:24 +0800

    fix: watch conner resin when full

--- ResinStatusWidget/LockScreenWidgets/LockScreenResinWidget/LockScreenResinWidgetCorner.swift
+++ ResinStatusWidget/LockScreenWidgets/LockScreenResinWidget/LockScreenResinWidgetCorner.swift
@@ -16,7 +16,7 @@ struct LockScreenResinWidgetCorner: View {
         switch result {
         case .success(let data):
             if data.resinInfo.isFull {
-                return String(format: NSLocalizedString("%lld, 已回满", comment: "resin"), data.resinInfo.currentResin)
+                return String(format: NSLocalizedString("160, 已回满", comment: "resin"))
             } else {
                 return "\(data.resinInfo.currentResin), \(data.resinInfo.recoveryTime.describeIntervalShort()), \(data.resinInfo.recoveryTime.completeTimePointFromNowShort())"
             }
