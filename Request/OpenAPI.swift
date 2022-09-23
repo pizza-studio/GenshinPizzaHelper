3888b5df2e2dddc486135851108096a1db9832a8
Bill-Haku <battleandwar@hotmail.com>
2022-09-23 20:07:39 +0800

    feat: add fetch player data open api

--- Request/OpenAPI.swift
+++ Request/OpenAPI.swift
@@ -11,10 +11,6 @@ extension API {
     struct OpenAPIs {
         /// 获取当前活动信息
         /// - Parameters:
-        ///     - region: 服务器地区
-        ///     - serverID: 服务器ID
-        ///     - uid: 用户UID
-        ///     - cookie: 用户Cookie
         ///     - completion: 数据
         static func fetchCurrentEvents (
             completion: @escaping (
@@ -27,6 +23,43 @@ extension API {
 
             // 请求
             HttpMethod<CurrentEvent>
+                .openRequest(
+                    .get,
+                    url
+                ) { result in
+                    switch result {
+
+                    case .success(let requestResult):
+                        print("request succeed")
+                        completion(.success(requestResult))
+
+                    case .failure(let requestError):
+                        switch requestError {
+                        case .decodeError(let message):
+                            completion(.failure(.decodeError(message)))
+                        default:
+                            completion(.failure(.requestError(requestError)))
+                        }
+                    }
+                }
+        }
+
+        /// 获取游戏内玩家详细信息
+        /// - Parameters:
+        ///     - uid: 用户UID
+        ///     - completion: 数据
+        static func fetchPlayerDatas (
+            _ uid: String,
+            completion: @escaping (
+                PlayerDetailsFetchResult
+            ) -> ()
+        ) {
+            // 请求类别
+            let urlStr = "https://enka.network/u/\(uid)/__data.json"
+            let url = URL(string: urlStr)!
+
+            // 请求
+            HttpMethod<PlayerDetails>
                 .openRequest(
                     .get,
                     url
