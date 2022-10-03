3cb52ab24c2491bafe7cef70a0b92b3a236b081c
Lava <2627403074@qq.com>
2022-10-03 10:56:06 +0800

    docs: Enka api model add docs

--- Model/EnkaAPIModel/PlayerDetailsModel.swift
+++ Model/EnkaAPIModel/PlayerDetailsModel.swift
@@ -8,43 +8,67 @@
 import Foundation
 
 struct PlayerDetails: Codable {
+    /// 账号基本信息
     var playerInfo: PlayerInfo
+    /// 正在展示的角色的详细信息
     var avatarInfoList: [AvatarInfo]?
     var ttl: Int
     var uid: String?
 
     struct PlayerInfo: Codable {
+        /// 名称
         var nickname: String
+        /// 等级
         var level: Int
+        /// 签名
         var signature: String
+        /// 世界等级
         var worldLevel: Int
+        /// 资料名片ID
         var nameCardId: Int
+        /// 已解锁成就数
         var finishAchievementNum: Int
+        /// 本期深渊乘数
         var towerFloorIndex: Int
+        /// 本期深渊间数
         var towerLevelIndex: Int
+        /// 正在展示角色信息列表（ID与等级）
         var showAvatarInfoList: [ShowAvatarInfo]
+        /// 正在展示名片ID的列表
         var showNameCardIdList: [Int]?
+        /// 玩家头像的角色的ID: profilePicture.avatarId
         var profilePicture: ProfilePicture
 
+
         struct ShowAvatarInfo: Codable {
+            /// 角色ID
             var avatarId: Int
+            /// 角色等级
             var level: Int
         }
 
         struct ProfilePicture: Codable {
+            /// 头像对应的角色ID
             var avatarId: Int
         }
     }
 
     struct AvatarInfo: Codable {
-
+        /// 角色ID
         var avatarId: Int
+        /// 角色属性
         var propMap: PropMap
+        /// 角色战斗属性
         var fightPropMap: FightPropMap
+        /// 角色天赋ID
         var skillDepotId: Int
+        /// 所有固定天赋ID的列表
         var inherentProudSkillList: [Int]
+        /// 天赋等级的字典，skillLevelMap.skillLevel: [天赋ID(String) : 等级(Int)]
         var skillLevelMap: SkillLevelMap
+        /// 装备列表，包括武器和圣遗物
         var equipList: [EquipList]
+        /// 角色好感等级，fetterInfo.expLevel
         var fetterInfo: FetterInfo
 
         struct PropMap: Codable {
@@ -79,32 +103,57 @@ struct PlayerDetails: Codable {
         }
 
         struct FightPropMap: Codable {
+            /// 基础生命
             var baseHP: Double
+            /// 基础攻击力
             var baseATK: Double
+            /// 基础防御力
             var baseDEF: Double
+            /// 暴击率
             var criticalRate: Double
+            /// 暴击伤害
             var criticalDamage: Double
+            /// 元素充能效率
             var energyRecharge: Double
+            /// 治疗加成
             var healingBonus: Double
+            /// 受治疗加成
             var healedBonus: Double
+            /// 元素精通
             var elementalMastery: Double
 
+            /// 物理抗性
             var physicalResistance: Double
+            /// 物理伤害加成
             var physicalDamage: Double
+            /// 火元素伤害加成
             var pyroDamage: Double
+            /// 雷元素伤害加成
             var electroDamage: Double
+            /// 水元素伤害加成
             var hydroDamage: Double
+            /// 草元素伤害加成
             var dendroDamage: Double
+            /// 风元素伤害加成
             var anemoDamage: Double
+            /// 岩元素伤害加成
             var geoDamage: Double
+            /// 冰元素伤害加成
             var cryoDamage: Double
 
+            /// 火元素抗性
             var pyroResistance: Double
+            /// 雷元素抗性
             var electroResistance: Double
+            /// 水元素抗性
             var hydroResistance: Double
+            /// 草元素抗性
             var dendroResistance: Double
+            /// 风元素抗性
             var anemoResistance: Double
+            /// 岩元素抗性
             var geoResistance: Double
+            /// 冰元素抗性
             var cryoResistance: Double
 
             var pyroEnergyCost: Double?
@@ -115,8 +164,11 @@ struct PlayerDetails: Codable {
             var cryoEnergyCost: Double?
             var geoEnergyCost: Double?
 
+            /// 生命值上限
             var HP: Double
+            /// 攻击力
             var ATK: Double
+            /// 防御力
             var DEF: Double
 
             private enum CodingKeys: String, CodingKey {
@@ -194,15 +246,22 @@ struct PlayerDetails: Codable {
             var weapon: Weapon?
             var flat: Flat
 
+            /// 圣遗物
             struct Reliquary: Codable {
+                /// 圣遗物等级
                 var level: Int
+                /// 圣遗物主属性ID
                 var mainPropId: Int
+                /// 圣遗物副属性ID的列表
                 var appendPropIdList: [Int]
             }
 
             struct Weapon: Codable {
+                /// 武器等级
                 var level: Int
+                /// 武器突破等级
                 var promoteLevel: Int?
+                /// 武器精炼等级（0-4）
                 var affixMap: AffixMap
 
                 struct AffixMap: Codable {
@@ -235,16 +294,23 @@ struct PlayerDetails: Codable {
             }
 
             struct Flat: Codable {
+                /// 装备名的哈希值
                 var nameTextMapHash: String
+                /// 圣遗物套装名称的哈希值
                 var setNameTextMapHash: String?
+                /// 装备稀有度
                 var rankLevel: Int
                 /// 圣遗物主词条
                 var reliquaryMainstat: ReliquaryMainstat?
-                /// 圣遗物副词条
+                /// 圣遗物副词条列表
                 var reliquarySubstats: [ReliquarySubstat]?
+                /// 武器属性列表：包括基础攻击力和副属性
                 var weaponStats: [WeaponStat]?
+                /// 装备类别：武器、圣遗物
                 var itemType: String
+                /// 装备名称图标
                 var icon: String
+                /// 圣遗物类型：花/羽毛/沙漏/杯子/头
                 var equipType: String?
 
                 struct ReliquaryMainstat: Codable {
