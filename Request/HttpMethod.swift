16189bc83232facbc7617113f1023768d6cd7c7b
Bill-Haku <battleandwar@hotmail.com>
2022-10-06 09:21:08 +0800

    fix: open request user agent

--- Request/HttpMethod.swift
+++ Request/HttpMethod.swift
@@ -506,6 +506,7 @@ struct HttpMethod<T: Codable> {
                     "Accept-Encoding": "gzip, deflate, br",
                     "Accept-Language": "zh-CN,zh-Hans;q=0.9",
                     "Accept": "application/json, text/plain, */*",
+                    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Safari/605.1.15",
                     "Connection": "keep-alive",
                 ]
                 // http方法
