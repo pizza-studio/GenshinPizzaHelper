a8255d5a23bdbcd5473d189c4844a32edf462e5b
Lava <2627403074@qq.com>
2022-11-19 16:17:12 +0800

    pref: live activity setting footer

--- GenshinPizzaHepler/View/SecondaryViews/LiveActivitySettingView.swift
+++ GenshinPizzaHepler/View/SecondaryViews/LiveActivitySettingView.swift
@@ -23,7 +23,7 @@ struct LiveActivitySettingView: View {
                 if !ResinRecoveryActivityController.shared.allowLiveActivity {
                     Text("实时活动功能未开启，请前往设置开启。")
                 } else {
-                    Text("若开启，在退出本App时会自动启用一个“实时活动”树脂计时器。您也可以在“概览”页长按某个账号的卡片手动开启。")
+                    Text("若开启，在退出本App时会自动启用一个“实时活动”树脂计时器。默认为顶置的账号，或树脂最少的账号开启计时器。您也可以在“概览”页长按某个账号的卡片手动开启，或启用多个计时器。")
                 }
 
             }
