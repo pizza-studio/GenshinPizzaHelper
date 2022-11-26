47f10ddfaf9c371df437c0da7c403e61755833fe
Lava <2627403074@qq.com>
2022-11-27 01:02:32 +0800

    pref: live activity setting

--- GenshinPizzaHepler/View/SecondaryViews/LiveActivitySettingView.swift
+++ GenshinPizzaHepler/View/SecondaryViews/LiveActivitySettingView.swift
@@ -8,32 +8,19 @@
 import SwiftUI
 #if canImport(ActivityKit)
 struct LiveActivitySettingView: View {
-    @AppStorage("autoDeliveryResinTimerLiveActivity") var autoDeliveryResinTimerLiveActivity: Bool = true
-
     @State var isAlertShow: Bool = false
+
     var body: some View {
         if #available(iOS 16.1, *) {
             Section {
-                Toggle("自动启用树脂计时器", isOn: $autoDeliveryResinTimerLiveActivity.animation())
-                    .disabled(!ResinRecoveryActivityController.shared.allowLiveActivity)
-                if !ResinRecoveryActivityController.shared.allowLiveActivity {
-                    Button("前往设置开启实时活动功能") {
-                        UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
-                    }
-                } else {
-                    NavigationLink("树脂计时器设置") {
-                        LiveActivitySettingDetailView()
-                    }
+                NavigationLink("树脂计时器设置") {
+                    LiveActivitySettingDetailView()
                 }
             } footer: {
-                if !ResinRecoveryActivityController.shared.allowLiveActivity {
-                    Text("实时活动功能未开启，请前往设置开启。")
-                } else {
-                    Button("树脂计时器是什么？") {
-                        isAlertShow.toggle()
-                    }
-                    .font(.footnote)
+                Button("树脂计时器是什么？") {
+                    isAlertShow.toggle()
                 }
+                .font(.footnote)
             }
             .alert("若开启，在退出本App时会自动启用一个“实时活动”树脂计时器。默认为顶置的账号，或树脂最少的账号开启计时器。您也可以在“概览”页长按某个账号的卡片手动开启，或启用多个计时器。", isPresented: $isAlertShow) {
                 Button("OK") {
@@ -46,6 +33,7 @@ struct LiveActivitySettingView: View {
 
 @available(iOS 16.1, *)
 struct LiveActivitySettingDetailView: View {
+    @Environment(\.scenePhase) var scenePhase
     @AppStorage("resinRecoveryLiveActivityUseEmptyBackground", store: UserDefaults(suiteName: "group.GenshinPizzaHelper")) var resinRecoveryLiveActivityUseEmptyBackground: Bool = false
 
     @AppStorage("resinRecoveryLiveActivityUseCustomizeBackground", store: UserDefaults(suiteName: "group.GenshinPizzaHelper")) var resinRecoveryLiveActivityUseCustomizeBackground: Bool = false
@@ -57,6 +45,7 @@ struct LiveActivitySettingDetailView: View {
         }
     }
 
+    @AppStorage("autoDeliveryResinTimerLiveActivity") var autoDeliveryResinTimerLiveActivity: Bool = true
 
     @AppStorage("resinRecoveryLiveActivityShowExpedition", store: UserDefaults(suiteName: "group.GenshinPizzaHelper")) var resinRecoveryLiveActivityShowExpedition: Bool = true
 
@@ -66,34 +55,51 @@ struct LiveActivitySettingDetailView: View {
 
     @State private var isHowToCloseDynamicIslandAlertShow: Bool = false
 
+    @State private var allowLiveActivity: Bool = ResinRecoveryActivityController.shared.allowLiveActivity
+
     var body: some View {
         List {
-            Section {
-                Button("如何隐藏灵动岛？如何关闭树脂计时器？") {
-                    isHowToCloseDynamicIslandAlertShow.toggle()
-                }
-            }
-            Section {
-                Toggle("展示派遣探索", isOn: $resinRecoveryLiveActivityShowExpedition)
-            }
-            Section {
-                Toggle("使用透明背景", isOn: $resinRecoveryLiveActivityUseEmptyBackground.animation())
-                if !resinRecoveryLiveActivityUseEmptyBackground {
-                    Toggle("随机背景", isOn: useRandomBackground.animation())
-                    if resinRecoveryLiveActivityUseCustomizeBackground {
-                        NavigationLink("选择背景") {
-                            LiveActivityBackgroundPicker()
-                        }
+            if !allowLiveActivity {
+                Section {
+                    Label {
+                        Text("实时活动功能未开启")
+                    } icon: {
+                        Image(systemName: "exclamationmark.circle")
+                            .foregroundColor(.red)
+                    }
+                    Button("前往设置开启实时活动功能") {
+                        UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
                     }
                 }
-            } header: {
-                Text("树脂计时器背景")
             }
-//            Section {
-//                Toggle("简洁模式下自动刷新树脂计时器", isOn: $autoUpdateResinRecoveryTimerUsingReFetchData)
-//            } footer: {
-//                Text("如出现1034问题，请关闭本选项。")
-//            }
+
+            Group {
+                Section {
+                    Toggle("自动启用树脂计时器", isOn: $autoDeliveryResinTimerLiveActivity.animation())
+                }
+                Section {
+                    Button("如何隐藏灵动岛？如何关闭树脂计时器？") {
+                        isHowToCloseDynamicIslandAlertShow.toggle()
+                    }
+                }
+                Section {
+                    Toggle("展示派遣探索", isOn: $resinRecoveryLiveActivityShowExpedition)
+                }
+                Section {
+                    Toggle("使用透明背景", isOn: $resinRecoveryLiveActivityUseEmptyBackground.animation())
+                    if !resinRecoveryLiveActivityUseEmptyBackground {
+                        Toggle("随机背景", isOn: useRandomBackground.animation())
+                        if resinRecoveryLiveActivityUseCustomizeBackground {
+                            NavigationLink("选择背景") {
+                                LiveActivityBackgroundPicker()
+                            }
+                        }
+                    }
+                } header: {
+                    Text("树脂计时器背景")
+                }
+            }
+            .disabled(!allowLiveActivity)
         }
         .toolbar {
             ToolbarItem {
@@ -119,6 +125,20 @@ struct LiveActivitySettingDetailView: View {
             }
         } message: {
             Text("您可以从左右向中间滑动灵动岛，即可隐藏灵动岛。\n在锁定屏幕上左滑树脂计时器，即可关闭树脂计时器和灵动岛。")
+        }
+        .onAppear {
+            withAnimation {
+                allowLiveActivity = ResinRecoveryActivityController.shared.allowLiveActivity
+            }
+        }
+        .onChange(of: scenePhase) { newValue in
+            if newValue == .active {
+                UNUserNotificationCenter.current().getNotificationSettings { settings in
+                    withAnimation {
+                        allowLiveActivity = ResinRecoveryActivityController.shared.allowLiveActivity
+                    }
+                }
+            }
         }
     }
 }
