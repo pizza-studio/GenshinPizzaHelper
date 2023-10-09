52dc18dc61c985ac890d0f8c4baa86ad96fa6a2b
ShikiSuen <shikisuen@outlook.com>
2023-10-10 01:29:03 +0800

    CharacterAsset // Add roundedRectangle support to .decoratedIcon().

--- Enums/Enum_CharacterAsset_SwiftUIImpl.swift
+++ Enums/Enum_CharacterAsset_SwiftUIImpl.swift
@@ -39,21 +39,23 @@ extension CharacterAsset {
     ///   - size: 尺寸。
     ///   - cutType: 决定裁切到哪个身体部位。
     /// - Returns: SwiftUI "some View"
+    @ViewBuilder
     public func decoratedIcon(
         _ size: CGFloat,
         cutTo cutType: DecoratedIconCutType = .shoulder,
         costume: CostumeAsset? = nil,
-        namecard givenNameCard: NameCard? = nil
+        namecard givenNameCard: NameCard? = nil,
+        roundRect: Bool = false
     )
         -> some View {
         let costume = costume ?? Self.costumeMap[self]
         // 由于 Lava 强烈反对针对证件照的脸裁切与头裁切，
         // 所以不预设启用该功能。
-        var cutType = cutType
-        if !Defaults[.cutShouldersForSmallAvatarPhotos] {
-            cutType = .shoulder
-        }
-        return EnkaWebIcon(iconString: costume?.frontPhotoFileName ?? frontPhotoFileName)
+        let cutType = !Defaults[.cutShouldersForSmallAvatarPhotos] ? .shoulder : cutType
+        let ratio = 179.649 / 1024
+        let cornerSize = CGSize(width: ratio * size, height: ratio * size)
+        let roundCornerSize = CGSize(width: size / 2, height: size / 2)
+        EnkaWebIcon(iconString: costume?.frontPhotoFileName ?? frontPhotoFileName)
             .scaledToFill()
             .frame(width: size * cutType.rawValue, height: size * cutType.rawValue)
             .clipped()
@@ -69,7 +71,7 @@ extension CharacterAsset {
                 }
             )
             .frame(width: size, height: size)
-            .clipShape(Circle())
-            .contentShape(Circle())
+            .clipShape(RoundedRectangle(cornerSize: roundRect ? cornerSize : roundCornerSize))
+            .contentShape(RoundedRectangle(cornerSize: roundRect ? cornerSize : roundCornerSize))
     }
 }
