2714443a10dab1bb9b6440ae8201f33ab57cd489
ShikiSuen <shikisuen@outlook.com>
2023-10-17 01:44:51 +0800

    ToolbarSharableInIOS16 // Fix for macOS.

--- CommonTools/ViewModifier/ToolbarSharableInIOS16.swift
+++ CommonTools/ViewModifier/ToolbarSharableInIOS16.swift
@@ -49,41 +49,35 @@ struct ToolbarSavePhotoButton<ViewToRender: View>: ViewModifier {
     var isAlertShow: Bool = false
 
     func body(content: Content) -> some View {
-        if OS.type == .macOS {
-            content
-        } else if #available(iOS 16, *) {
+        if #available(iOS 16, *) {
             content
                 .toolbar {
                     ToolbarItem(placement: placement) {
-//                        ShareLink(
-//                            item: Image(uiImage: image),
-//                            preview: SharePreview("分享", image: Image(uiImage: image))
-//                        )
-                        Button {
-                            isAlertShow = true
-                        } label: {
-                            Image(systemSymbol: .squareAndArrowDown)
+                        if let image = getImage() {
+                            let outputImage = Image(uiImage: image)
+                            ShareLink(
+                                item: outputImage,
+                                preview: SharePreview(title, image: outputImage)
+                            )
                         }
                     }
-                }
-                .alert(title, isPresented: $isAlertShow) {
-                    Button("OK") {
-                        let renderer = ImageRenderer(
-                            content: viewToRender
-                                .environment(
-                                    \.locale,
-                                    .init(identifier: Locale.current.identifier)
-                                )
-                        )
-                        renderer.scale = UIScreen.main.scale
-                        if let image = renderer.uiImage {
-                            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
-                        }
-                    }
-                    Button("取消", role: .cancel) {}
                 }
         } else {
             content
         }
+    }
+
+    @available(iOS 16.0, *)
+    @MainActor
+    func getImage() -> UIImage? {
+        let renderer = ImageRenderer(
+            content: viewToRender
+                .environment(
+                    \.locale,
+                    .init(identifier: Locale.current.identifier)
+                )
+        )
+        renderer.scale = UIScreen.main.scale
+        return renderer.uiImage
     }
 }
