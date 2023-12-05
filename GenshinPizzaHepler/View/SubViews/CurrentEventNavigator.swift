4e2a139c293ed7c0cba158f1cdf480fe1aff1f26
ShikiSuen <shikisuen@outlook.com>
2023-12-06 01:41:56 +0800

    CurrentEventNavigator // iOS compatibility.

--- GenshinPizzaHepler/View/SubViews/CurrentEventNavigator.swift
+++ GenshinPizzaHepler/View/SubViews/CurrentEventNavigator.swift
@@ -74,7 +74,7 @@ struct CurrentEventNavigator: View {
                         }
                     }
                 }
-                .padding(.vertical, OS.type == .macOS ? UIFont.systemFontSize : nil)
+                .padding(.vertical, OS.type == .macOS ? UIFont.systemFontSize : 0)
             } header: {
                 NavigationLink {
                     AllEventsView(eventContents: $eventContents)
