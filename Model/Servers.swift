e3cefcf4b4ccc5212ccd8c66fbad94f7eadf6c8f
ShikiSuen <shikisuen@outlook.com>
2024-03-16 22:55:25 +0800

    AccountConfiguration // Dynamically give the correct fallback server.

--- Model/Servers.swift
+++ Model/Servers.swift
@@ -13,14 +13,31 @@ import HoYoKit
 extension AccountConfiguration {
     var server: Server {
         get {
-            if let serverRawValue {
-                Server(rawValue: serverRawValue) ?? .china
-            } else {
-                Server.china
-            }
+            Server(rawValue: serverRawValue ?? "") ?? fallbackServer
         }
         set {
             serverRawValue = newValue.rawValue
+        }
+    }
+
+    var fallbackServer: Server {
+        Self.getFallbackServer(from: uid) ?? .china
+    }
+
+    public static func getFallbackServer(from uid: String?) -> Server? {
+        guard var theUID = uid else { return nil }
+        while theUID.count > 9 {
+            theUID = theUID.dropFirst().description
+        }
+        guard let initial = theUID.first, let initialInt = Int(initial.description) else { return nil }
+        switch initialInt {
+        case 1 ... 4: return .china
+        case 5: return .bilibili
+        case 6: return .unitedStates
+        case 7: return .europe
+        case 8: return .asia
+        case 9: return .hongKongMacauTaiwan
+        default: return nil
         }
     }
 }
