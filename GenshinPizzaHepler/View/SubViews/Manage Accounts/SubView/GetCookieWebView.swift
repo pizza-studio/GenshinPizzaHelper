153e70cacaaee5d8e965011cafc58c182242b736
ShikiSuen <shikisuen@outlook.com>
2024-05-20 13:20:54 +0800

    QRCode // Use CGImage instead.

--- GenshinPizzaHepler/View/SubViews/Manage Accounts/SubView/GetCookieWebView.swift
+++ GenshinPizzaHepler/View/SubViews/Manage Accounts/SubView/GetCookieWebView.swift
@@ -252,7 +252,7 @@ struct QRCodeGetCookieView: View {
                             viewModel.reCreateQRCode()
                         }
                     } else if let qrCodeAndTicket = viewModel.qrCodeAndTicket {
-                        Image(uiImage: qrCodeAndTicket.qrCode)
+                        Image(decorative: qrCodeAndTicket.qrCode, scale: 1)
                             .interpolation(.none)
                             .resizable()
                             .scaledToFit()
@@ -367,7 +367,7 @@ class QRCodeGetCookieViewModel: ObservableObject {
     static var shared: QRCodeGetCookieViewModel = .init()
 
     @Published
-    var qrCodeAndTicket: (qrCode: UIImage, ticket: String)?
+    var qrCodeAndTicket: (qrCode: CGImage, ticket: String)?
     @Published
     var taskId: UUID
     @Published
