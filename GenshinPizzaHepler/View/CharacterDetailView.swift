16e4c945746fc2149866615cddf50e7d5715f71e
ShikiSuen <shikisuen@outlook.com>
2024-05-20 12:23:46 +0800

    CDV // Amend the previous background fix.

--- GenshinPizzaHepler/View/CharacterDetailView.swift
+++ GenshinPizzaHepler/View/CharacterDetailView.swift
@@ -77,6 +77,7 @@ struct CharacterDetailView: View {
             }
         }
         .clipped()
+        .compositingGroup()
         .addWaterMark(showWaterMark)
         .onChange(of: showingCharacterName) { _ in
             simpleTaptic(type: .selection)
