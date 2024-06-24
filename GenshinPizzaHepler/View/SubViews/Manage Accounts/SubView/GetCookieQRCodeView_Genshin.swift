74c711dc88b35f0e6bb473f896600def58360d18
ShikiSuen <shikisuen@outlook.com>
2024-06-24 22:43:37 +0800

    QRCode // Refactor the process of parsing game token.

---
+++ GenshinPizzaHepler/View/SubViews/Manage Accounts/SubView/GetCookieQRCodeView_Genshin.swift
@@ -0,0 +1,14 @@
+// (c) 2022 and onwards Pizza Studio (GPL v3.0 License).
+// ====================
+// This code is released under the GPL v3.0 License (SPDX-License-Identifier: GPL-3.0)
+
+import HBMihoyoAPI
+
+extension GetCookieQRCodeView {
+    func extraCookieProcess(cookie: inout String) async throws {
+        print(cookie.count)
+        // Do nothing in PizzaHelper4Genshin for now.
+        // This file is to make sure that the GetCookieQRCodeView.swift file consistent
+        // with the same file in PizzaHelper4HSR.
+    }
+}
