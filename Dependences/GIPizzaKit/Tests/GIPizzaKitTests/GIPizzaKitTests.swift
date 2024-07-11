91f209c447e172a07b3c66d144f72f8b160f6328
ShikiSuen <shikisuen@outlook.com>
2024-07-11 18:35:44 +0800

    GIPizzaKit // +.testCharacterArtifactRatingModelValidity().

--- Dependences/GIPizzaKit/Tests/GIPizzaKitTests/GIPizzaKitTests.swift
+++ Dependences/GIPizzaKit/Tests/GIPizzaKitTests/GIPizzaKitTests.swift
@@ -29,4 +29,11 @@ final class GIPizzaKitTests: XCTestCase {
             print(x)
         }
     }
+
+    func testCharacterArtifactRatingModelValidity() throws {
+        // 这个测试用来检查是否有角色的圣遗物评分数据出现了重复的 key。
+        // Swift 的编译过程不会阻止这种情况，所以这种情况会导致 runtime error。
+        // 于是只能写这么个单元测试。
+        _ = CharacterAsset.allCases.map { $0.getArtifactRatingModel() }
+    }
 }
