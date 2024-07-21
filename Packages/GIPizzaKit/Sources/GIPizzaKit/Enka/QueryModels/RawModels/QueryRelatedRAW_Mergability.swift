70284f500001533778843148eb35aa98363131d2
ShikiSuen <shikisuen@outlook.com>
2024-07-21 09:53:17 +0800

    GIPizzaKit // Implementing mergability to ProfileRAW.

--- Packages/GIPizzaKit/Sources/GIPizzaKit/Enka/QueryModels/RawModels/QueryRelatedRAW_Mergability.swift
+++ Packages/GIPizzaKit/Sources/GIPizzaKit/Enka/QueryModels/RawModels/QueryRelatedRAW_Mergability.swift
@@ -1,3 +1,41 @@
 // (c) 2022 and onwards Pizza Studio (GPL v3.0 License).
 // ====================
 // This code is released under the GPL v3.0 License (SPDX-License-Identifier: GPL-3.0)
+
+extension EnkaGI.QueryRelated.ProfileRAW {
+    public func merge(new: Self) -> Self {
+        guard let oldAvatars = avatarInfoList,
+              var newAvatars = new.avatarInfoList
+        else { return self }
+        let existingCharIds: [Int] = newAvatars.map(\.avatarId)
+        oldAvatars.forEach { currentOldChar in
+            guard !existingCharIds.contains(currentOldChar.avatarId) else { return }
+            newAvatars.append(currentOldChar)
+        }
+        return .init(
+            playerInfo: playerInfo,
+            avatarInfoList: newAvatars,
+            ttl: ttl,
+            uid: uid,
+            message: message
+        )
+    }
+
+    public func merge(old: Self?) -> Self {
+        guard let oldAvatars = old?.avatarInfoList,
+              var newAvatars = avatarInfoList
+        else { return self }
+        let existingCharIds = newAvatars.map(\.avatarId)
+        oldAvatars.forEach { currentOldChar in
+            guard !existingCharIds.contains(currentOldChar.avatarId) else { return }
+            newAvatars.append(currentOldChar)
+        }
+        return .init(
+            playerInfo: playerInfo,
+            avatarInfoList: newAvatars,
+            ttl: ttl,
+            uid: uid,
+            message: message
+        )
+    }
+}
