ac559b70d7316d4330b7c4d40af99ffd34f96729
Lava <34743145+CanglongCl@users.noreply.github.com>
2024-09-17 12:42:42 -0700

    fix: cannot launch widget xcode 16

--- ResinStatusWidget/WidgetsBoundle.swift
+++ ResinStatusWidget/WidgetsBoundle.swift
@@ -8,47 +8,25 @@
 import SwiftUI
 import WidgetKit
 
-#if !os(watchOS)
-struct WidgetsBundleLowerThaniOS16: WidgetBundle {
-    var body: some Widget {
-        MainWidget()
-        MaterialWidget()
-    }
-}
-#endif
-
 // MARK: - WidgetsBundleiOS16
 
 @available(iOSApplicationExtension 16.0, watchOSApplicationExtension 9.0, *)
 struct WidgetsBundleiOS16: WidgetBundle {
     var body: some Widget {
-        #if !os(watchOS)
-        WidgetsBundleLowerThaniOS16().body
         #if canImport(ActivityKit)
         if #available(iOS 16.1, *) {
             ResinRecoveryActivityWidget()
         }
         #endif
-        #else
-        AlternativeWatchCornerResinWidget()
+        #if !os(watchOS)
+        MainWidget()
+        MaterialWidget()
         #endif
         LockScreenResinWidget()
         LockScreenLoopWidget()
         LockScreenAllInfoWidget()
-        SubWidgetsBundleiOS16().body
-    }
-}
-
-// MARK: - SubWidgetsBundleiOS16
-
-// 因为一个body只能放10个
-@available(iOSApplicationExtension 16.0, watchOSApplicationExtension 9.0, *)
-struct SubWidgetsBundleiOS16: WidgetBundle {
-    var body: some Widget {
-//        #if !os(watchOS)
         LockScreenResinTimerWidget()
         LockScreenResinFullTimeWidget()
-//        #endif
         LockScreenHomeCoinWidget()
         LockScreenDailyTaskWidget()
         LockScreenExpeditionWidget()
